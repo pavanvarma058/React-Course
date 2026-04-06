c793351dc0d03a69979dce1882ba5596f81a5523
Pavan Varma <pavanvarma058@gmail.com>
2026-04-06 21:48:13 +0100

    Completed the state challenge

---
+++ state-challenge-1/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(0);
+  const [count, setCount] = useState(0);
+
+  const date = new Date("April 06, 2026");
+  date.setDate(date.getDate() + count);
+  return (
+    <div>
+      <h1>TEST</h1>
+      <div>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
+        <span>Step: {step}</span>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <span>Count: {count}</span>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+  );
+}
+
+export default App;
